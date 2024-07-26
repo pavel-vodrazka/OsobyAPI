11bcddc2fc74b2d3ad1ff11912e845af0256a65d
Pavel Vodrážka <Pavel.Vodrazka@gmail.com>
2024-07-26 09:49:36 +0200

    Add project files.

---
+++ OsobyApi/App_Start/AppPreStart.cs
@@ -0,0 +1,18 @@
+﻿using Serilog;
+using XXX;
+
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(AppPreStart), nameof(AppPreStart.PreApplicationStart))]
+namespace XXX
+{
+    /// <summary>
+    /// This runs even before global.asax Application_Start (see WebActivatorConfig)
+    /// </summary>
+    public class AppPreStart
+    {
+        public static void PreApplicationStart()
+        {
+            LogConfig.Configure();
+        }
+    }
+}
---
+++ OsobyApi/App_Start/LogConfig.cs
@@ -0,0 +1,36 @@
+﻿using Serilog;
+using Serilog.Events;
+using SerilogWeb.Classic;
+using SerilogWeb.Classic.Enrichers;
+using SerilogWeb.Classic.WebApi.Enrichers;
+using System;
+
+namespace XXX
+{
+    public class LogConfig
+    {
+        public static void Configure()
+        {
+            ApplicationLifecycleModule.LogPostedFormData = LogPostedFormDataOption.OnlyOnError;
+            //SerilogWebClassic.Configure(cfg => cfg.EnableFormDataLogging());
+            ApplicationLifecycleModule.FormDataLoggingLevel = LogEventLevel.Debug;
+            //SerilogWebClassic.Configure(cfg => cfg.EnableFormDataLogging(forms => forms.AtLevel(LogEventLevel.Debug)));
+            ApplicationLifecycleModule.RequestLoggingLevel = LogEventLevel.Debug;
+            //SerilogWebClassic.Configure(cfg => cfg.LogAtLevel(LogEventLevel.Debug));
+
+            Environment.SetEnvironmentVariable("BASEDIR", AppDomain.CurrentDomain.BaseDirectory);
+
+            var loggerConfiguration = new LoggerConfiguration().ReadFrom.AppSettings()
+                    .Enrich.FromLogContext()
+                    .Enrich.With<HttpRequestIdEnricher>()
+                    .Enrich.With<UserNameEnricher>()
+                    .Enrich.With<HttpRequestUrlEnricher>()
+                    .Enrich.With<WebApiRouteTemplateEnricher>()
+                    .Enrich.With<WebApiControllerNameEnricher>()
+                    .Enrich.With<WebApiRouteDataEnricher>()
+                    .Enrich.With<WebApiActionNameEnricher>();
+
+            Log.Logger = loggerConfiguration.CreateLogger();
+        }
+    }
+}
